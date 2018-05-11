ea14b719a74815e3daa9d62ce298936172a79e00
Unknown <nikey646@users.noreply.github.com>
2018-05-11 15:47:32 +1000

    Moved VndbSharp into the src folder
    Moved VndbConsole(Core) into the samples folder

--- src/VndbSharp/AssemblyInfo.cs
+++ src/VndbSharp/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
+// These are only used for the StringExtensions.Quote() extension method #Lazy
 [assembly: InternalsVisibleTo("VndbConsole"),
 		   InternalsVisibleTo("VndbConsoleCore")]
